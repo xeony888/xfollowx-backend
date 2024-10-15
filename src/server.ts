900e8e8d7c14cdd8f24116d912c6b081f494495b
josh handelman <handelmanjos@gmail.com>
2024-10-15 12:26:42 +0100

    stuff

--- src/server.ts
+++ src/server.ts
@@ -62,9 +62,8 @@ app.post("/helio", async (req, res) => {
                 console.log(`Server not found: ${serverId}`);
                 return res.status(404).json({ error: "Not found" });
             }
-            console.log(discordUser);
             if (server.owner.discordName !== discordUser.username) {
-                console.log(`Incorrect owner: ${server.owner.discordName, discordUser.username}`);
+                console.log(`Incorrect owner: ${server.owner.discordName}, ${discordUser.username}`);
                 return res.status(401).json({ error: "Unauthorized" });
             }
             let updatedServer: any;
